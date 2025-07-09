d36cac1dadb0f8e35c32577f1b411bcd0cbf49e5
Dedeco <andresousa@google.com>
2025-07-09 16:40:37 -0300

    Update bq_start_demo.sql

--- bq_start_demo.sql
+++ bq_start_demo.sql
@@ -34,6 +34,37 @@ values
   (3, 'Dot', 'Acme'),
   (4, 'Bill', 'Initech');
 
+
+-- Create and populate the 'pageviews' table
+-- This table is denormalized and contains event data for pageviews.
+CREATE OR REPLACE TABLE demo.pageviews (
+  event_date DATE,
+  product STRING,
+  user_id INT64
+);
+
+INSERT INTO demo.pageviews (event_date, product, user_id)
+VALUES
+  (DATE('2017-01-01'), 'Rockets', 1),
+  (DATE('2017-01-01'), NULL, 2),
+  (DATE('2017-03-15'), 'Portable Holes', 7423);
+
+
+-- Create and populate the 'orders' table
+-- This table is denormalized and contains transaction data.
+CREATE OR REPLACE TABLE demo.orders (
+  order_date DATE,
+  product STRING,
+  amount NUMERIC,
+  user_id INT64
+);
+
+INSERT INTO demo.orders (order_date, product, amount, user_id)
+VALUES
+  (DATE('2017-01-01'), 'Rockets', 200, 37),
+  (DATE('2017-01-02'), 'Synergy', 1000, 46),
+  (DATE('2017-03-15'), 'Portable Holes', 50, 4910);
+
 -- query to produce the fanned-out result
 select
   a.id as account_id,
